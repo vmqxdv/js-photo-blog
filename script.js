eccc498cc2f3c87eba7a018472135b0c0b6578a1
vmqxdv <vquintilio@gmail.com>
2025-03-17 09:37:11 +0100

    feat: Dichiarate variabili principali

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API,
+ * sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+ * 
+ */
+
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/';
+const rowElement = document.querySelector('.row');
